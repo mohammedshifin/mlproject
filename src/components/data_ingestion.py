6940a0c98ab2801615a9aa1f36fff9b3981c5662
mohammed shifin <mohammedshifin1521@gmail.com>
2025-01-11 09:16:23 +0530

    Model Trained

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -5,6 +5,7 @@ from src.logger import logging
 from src.components.data_transformation import DataTransformation,DataTransformationConfig
 import pandas as pd
 from pathlib import Path
+from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
@@ -50,4 +51,7 @@ if __name__=="__main__":
     obj = DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
     data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer = ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
--- src/utils.py
+++ src/utils.py
@@ -4,14 +4,36 @@ import dill
 import numpy as np
 import pandas as pd
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import r2_score
 
-def save_object(file_path,obj):
+def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
-
-        os.makedirs(dir_path,exist_ok=True)
-
-        with open(file_path,'wb') as file_obj:
-            dill.dump(obj,file_obj)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            dill.dump(obj, file_obj)
     except Exception as e:
-        raise CustomException(e,sys)
+        raise CustomException(e, sys)
+
+def evaluate_models(X_train, Y_train, X_test, Y_test, models, params):
+    try:
+        report = {}
+        for model_name, model in models.items(): # Iterate directly through models.items()
+            para = params[model_name] # Access params using the model name
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, Y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, Y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(Y_train, y_train_pred)
+            test_model_score = r2_score(Y_test, y_test_pred)
+
+            report[model_name] = test_model_score # Use model_name as the key
+        return report
+    except Exception as e:
+        raise CustomException(e, sys)
